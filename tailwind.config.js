2543241f0a45afd9c943ba7ffa30c3c68bb002a0
c9addy <singh.aditya44@gmail.com>
2020-12-20 13:54:55 +0530

    test2

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./pages/**/*.js', './components/**/*.js', './pages/**/*.ts', './components/**/*.ts',],
+  purge: ['./pages/**/*.js', './components/**/*.js', './**/*.ts', './components/**/*.ts',],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
